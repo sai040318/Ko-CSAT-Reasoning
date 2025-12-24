285ad0b45c07b853edac85787ecc8fe6bd91917b
ttotto0421 <ttotto0421@gmail.com>
2025-12-24 05:19:39 +0000

    chore: ignore, requirements 추가

--- src/model/unsloth_model.py
+++ src/model/unsloth_model.py
@@ -34,7 +34,7 @@ class UnslothModel(BaseModel):
         tokenizer.chat_template = "{% if messages[0]['role'] == 'system' %}{% set system_message = messages[0]['content'] %}{% endif %}{% if system_message is defined %}{{ system_message }}{% endif %}{% for message in messages %}{% set content = message['content'] %}{% if message['role'] == 'user' %}{{ '<start_of_turn>user\n' + content + '<end_of_turn>\n<start_of_turn>model\n' }}{% elif message['role'] == 'assistant' %}{{ content + '<end_of_turn>\n' }}{% endif %}{% endfor %}"
         
         return tokenizer
-
+    
     def __init__(self, model_name_or_path: str, **kwargs):
         super().__init__(model_name_or_path, **kwargs)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
